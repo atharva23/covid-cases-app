37eeaab0255d26471bd8efa65ab6e10472072db3
atharva23 <atharva.sayankar@gmail.com>
2021-06-07 15:00:18 +0530

    added

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="txt-centre bg-blue-800 text-white p-4 mb-10">
+
+    <div class="text-3xl md:text-5xl font-bold"><i class="fa fa-viruses"></i>covid 19 app</div>
+
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  
+};
+</script>
+
+
